1281935681fe434575c73237853ec0449f9406e1
kazasaki23311 <julia.nikashina@gmail.com>
2026-06-09 22:49:36 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -17,17 +17,21 @@ class PMTree {
   void buildTree(std::shared_ptr<Node> node, std::vector<char> remaining);
   void getAllPermutations(std::shared_ptr<Node> node, std::vector<char>& current,
                           std::vector<std::vector<char>>& result);
-  bool navigateToPermutation(std::shared_ptr<Node> node,
-                             std::vector<char>& result,
-                             int& remainingNum,
-                             const std::vector<char>& originalElements);
  public:
   explicit PMTree(const std::vector<char>& elements);
   ~PMTree() = default;
   std::vector<std::vector<char>> getAllPerms();
-  std::vector<char> getPerm1(int num);
-  std::vector<char> getPerm2(int num);
   static int factorial(int n);
   std::vector<char> getOriginalElements() const { return originalElements; }
+  friend std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+  friend std::vector<char> getPerm1(PMTree& tree, int num);
+  friend std::vector<char> getPerm2(PMTree& tree, int num);
+  friend bool navigateToPermutation(std::shared_ptr<Node> node,
+                                    std::vector<char>& result,
+                                    int& remainingNum,
+                                    const std::vector<char>& originalElements);
 };
+std::vector<std::vector<char>> getAllPerms(PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
 #endif  // INCLUDE_TREE_H_
